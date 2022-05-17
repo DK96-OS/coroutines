bfe680910c6f4394428785b570e75610993dfa7e
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-05-17 11:25:19 -0400

    Code Cleanup: * Update CoroutineQueueTest - replace private data classes with Example Test data, improve code style

--- queue/src/test/kotlin/coroutines/queue/CoroutineQueueTest.kt
+++ queue/src/test/kotlin/coroutines/queue/CoroutineQueueTest.kt
@@ -1,5 +1,7 @@
 package coroutines.queue
 
+import coroutines.examples.InputData
+import coroutines.examples.OutputData
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
@@ -17,29 +19,14 @@ import kotlin.random.Random
  */
 class CoroutineQueueTest {
 
-	/** Data structure used as coroutine input for tests */
-	private class ProcessInput(
-		val stream: ByteArray,
-		val key: Byte
-	)
-
-	/** Data structure for coroutine output */
-	private class ProcessOutput(
-		val title: String,
-		val index: Int
-	) {
-		/** Reveals when this object is created */
-		val createTime: Long = System.nanoTime()
-	}
-
 	/** The capacity is the size of the input list */
 	private val capacity = 1000
 
-	private lateinit var q1: CoroutineQueue<ProcessOutput>
+	private lateinit var queue: CoroutineQueue<OutputData>
 
   	/** Generate immutable input data to be used in all tests */
-	private val inputList: List<ProcessInput> = Array(capacity) {
-        ProcessInput(
+	private val inputList: List<InputData> = Array(capacity) {
+	    InputData(
 			stream = Random
 				.nextBytes(64)
 				.sortedArray(),	// sort to avoid string init issues
@@ -52,7 +39,7 @@ class CoroutineQueueTest {
 
 	@BeforeEach
 	fun setup() {
-		q1 = CoroutineQueue(capacity)
+		queue = CoroutineQueue(capacity)
     }
 
 	@Test
@@ -65,11 +52,11 @@ class CoroutineQueueTest {
 	fun testAwaitList() {
 		val output = runBlocking {
 			println("Loadin Queue: ${System.nanoTime()}")
-			for (i in inputList) q1.add(async {
-				ProcessOutput(String(i.stream), i.key.toInt())
+			for (i in inputList) queue.add(async {
+				i.transform()
 			})
 			println("Filled Queue: ${System.nanoTime()}")
-			q1.awaitList()	// returns a list of the output type
+			queue.awaitList()	// returns a list of the output type
 		}
 		println("Output ready: ${System.nanoTime()}")
 		assertEquals(
@@ -96,57 +83,51 @@ class CoroutineQueueTest {
 	@Test
     fun testAwaitNext() {
 		runBlocking {
-			for (i in inputList) q1.add(async {
-				ProcessOutput(
-					String(i.stream), i.key.toInt()
-				)
+			for (i in inputList) queue.add(async {
+				i.transform()
 			})
-			var counter = q1.count	// Count down to zero
+			var counter = queue.count	// Count down to zero
 			while (counter-- > 0)
 				assertNotNull(
-					q1.awaitNext())
+					queue.awaitNext())
 			assertNull(
-				q1.awaitNext())
+				queue.awaitNext())
 		}
 	}
 
 	@Test
     fun testCancel() {
 		runBlocking {
-			for (i in inputList) q1.add(async {
-				ProcessOutput(
-					String(i.stream), i.key.toInt()
-				)
+			for (i in inputList) queue.add(async {
+				i.transform()
 			})
 			assertEquals(
-				inputList[0].key.toInt(),
-				q1.awaitNext()!!.index
+				inputList[0].key,
+				queue.awaitNext()!!.key
 			)
 			assertEquals(
-				capacity - 1, q1.count)
-			q1.cancel(
+				capacity - 1, queue.count)
+			queue.cancel(
 				CancellationException("Testing Cancel Operation"))
 			assertEquals(
-				0, q1.count)
+				0, queue.count)
 			assertNull(
-				q1.awaitNext())
+				queue.awaitNext())
 		}
 	}
 
 	@Test
     fun testReusability() {
 		runBlocking {
-			for (i in inputList) q1.add(async {
-				ProcessOutput(
-					String(i.stream), i.key.toInt())
+			for (i in inputList) queue.add(async {
+				i.transform()
 			})
-			q1.cancel()
+			queue.cancel()
 				// Now Retry
-			for (i in inputList) q1.add(async {
-				ProcessOutput(
-					String(i.stream), i.key.toInt())
+			for (i in inputList) queue.add(async {
+				i.transform()
 			})
-			val output = q1.awaitList()
+			val output = queue.awaitList()
 			assertEquals(
 				capacity, output.size)
 			for (out in output)
@@ -160,8 +141,7 @@ class CoroutineQueueTest {
 		runBlocking {
 			val output = CoroutineQueue.transformList(inputList) {
 				delay(20)
-				ProcessOutput(
-					String(it.stream), it.key.toInt())
+				it.transform()
 			}
 			assertEquals(
 				capacity, output.size)
@@ -177,7 +157,9 @@ class CoroutineQueueTest {
 			val nullTestInputs = listOf(
 				1, 2, 3, 4, 5, 6
 			)
-			val output = CoroutineQueue.transformList(nullTestInputs) {
+			val output = CoroutineQueue.transformList(
+				nullTestInputs
+			) {
 				when {
 					it % 2 == 0 -> "Even"
 					it % 3 == 0 -> "Three"
