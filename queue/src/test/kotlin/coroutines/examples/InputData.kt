a40ae01ff7e81527413d1d2301c3331464504c3c
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2022-07-16 17:59:34 -0400

    Release 0.3.4 (#14)
    Build Update:
    * Update Artifact 0.3.3 -> 0.3.4
    * Update Kotlin 1.6.21 -> 1.7.10
    * Update Dokka 1.6.10 -> 1.7.10
    
    Test:
    * Update InputData - create cpuIntensiveTransformation method

--- queue/src/test/kotlin/coroutines/examples/InputData.kt
+++ queue/src/test/kotlin/coroutines/examples/InputData.kt
@@ -1,5 +1,7 @@
 package coroutines.examples
 
+import kotlin.math.roundToInt
+
 /** Data structure used as coroutine input for tests.
  * @author DK96-OS : 2022
  */
@@ -18,5 +20,35 @@ class InputData(
 	else OutputData(
 		key, String(stream)
 	)
+
+	/** Perform a slow and cpu intensive operation on the input data.
+	 */
+	fun cpuIntensiveTransformation()
+		: OutputData {
+		var variable1 = 0
+		var variable2 = 0
+		// Outer Loop
+		for (i in stream) {
+			// Inner Loop 1
+			for (j in stream) {
+				if (i == key) {
+					variable1 += ((i + (j * key) / 767f) * 20).roundToInt()
+				} else {
+					variable1 -= ((j * key) / 9008f).roundToInt()
+				}
+			}
+			// Inner Loop 2
+			for (k in stream) {
+				if (k == key) {
+					variable2 += ((i + (k * key) / 1767f) * 3).roundToInt()
+				} else {
+					variable2 -= ((k * key) / 9108f).roundToInt()
+				}
+			}
+		}
+		return OutputData(
+			key, "$variable1 - $variable2"
+		)
+	}
 
 }
